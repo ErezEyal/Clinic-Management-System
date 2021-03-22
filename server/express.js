054138c72bb4d37ebc83129226497c8f3d14b565
Erez Eyal <erez.eyal07@gmail.com>
2021-03-22 03:31:07 +0200

    added ciphers

--- server/express.js
+++ server/express.js
@@ -122,7 +122,16 @@ if (process.argv[2] === "prod") {
     cert: fs.readFileSync(
       "/etc/letsencrypt/live/app.barelclinic.com/fullchain.pem"
     ),
-    ca: fs.readFileSync('/etc/letsencrypt/live/app.barelclinic.com/chain.pem'),
+    ca: fs.readFileSync("/etc/letsencrypt/live/app.barelclinic.com/chain.pem"),
+    ciphers: [
+      "ECDHE-RSA-AES128-SHA256",
+      "DHE-RSA-AES128-SHA256",
+      "AES128-GCM-SHA256",
+      "RC4",
+      "HIGH",
+      "!MD5",
+      "!aNULL",
+    ].join(":"),
     port: process.argv[3],
   };
   https.createServer(app).listen(options, async () => {
